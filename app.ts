1dc622810b6d288c9dc86576343191209da73740
pravinshedbale <pravin.shedbale@outlook.com>
2022-04-04 20:07:18 +0530

    type number implemented

--- app.ts
+++ app.ts
@@ -1,8 +1,8 @@
-function add(n1, n2) {
+function add(n1: number, n2: number) {
     return n1 + n2;
 }
 
-const number1 = "10";
+const number1 = 10;
 const number2 = 20.5;
 
 const result = add(number1, number2);
