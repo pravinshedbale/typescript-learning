8c88e03c8ed2fffb2f03b5384481ffac3460ef94
pravinshedbale <pravin.shedbale@outlook.com>
2022-04-05 07:21:36 +0530

    Type inference

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
-function add(n1, n2) {
-    return n1 + n2;
+function add(n1, n2, showResult, resultPhrase) {
+    if (showResult) {
+        alert(n1 + n2);
+    }
+    else {
+        alert(resultPhrase);
+    }
 }
 var number1 = 10;
 var number2 = 20.5;
-var result = add(number1, number2);
-console.log(result);
+var showResult = false;
+var resultPhrase = "You have selected not to log result .....";
+var result = add(number1, number2, showResult, resultPhrase);
--- app.ts
+++ app.ts
@@ -1,10 +1,19 @@
-function add(n1: number, n2: number) {
-    return n1 + n2;
+function add(
+    n1: number,
+    n2: number,
+    showResult: boolean,
+    resultPhrase: string
+) {
+    if (showResult) {
+        alert(n1 + n2);
+    } else {
+        alert(resultPhrase);
+    }
 }
 
 const number1 = 10;
 const number2 = 20.5;
 
-const result = add(number1, number2);
-
-console.log(result);
+const showResult = false;
+const resultPhrase = "You have selected not to log result .....";
+const result = add(number1, number2, showResult, resultPhrase);
